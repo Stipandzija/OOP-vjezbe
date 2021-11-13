eb47b82a967ca08ea0f28e9776cfcdfbed171b6e
pasko <stipandzijapasko@gmail.com>
2021-11-14 00:07:35 +0100

    Vjezba_1

---
+++ Vjezba_3/Vjezba_3/Header.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <cstring>
+#include <string>
+using std::vector;
+using std::string;
+
+vector<int> input(vector<int>& arr, int n);
+vector<int> input2(vector<int>& arr, int x, int y);
+void print(vector<int>& arr);
+vector<int> input3(vector<int>& arr1, vector<int>& arr2);
+int suma(vector<int>& v);
+vector<int> erase(vector<int>& v);
+
+size_t count_s(const string &s, const string &s2);
