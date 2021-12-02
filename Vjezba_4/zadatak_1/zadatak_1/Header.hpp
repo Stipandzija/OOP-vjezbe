6ebf9e518fefd796b4e6fac5d95bd4ac70ee69f8
pasko <stipandzijapasko@gmail.com>
2021-12-02 05:50:30 +0100

    Vjezba_1

---
+++ Vjezba_4/zadatak_1/zadatak_1/Header.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void razdvaja(vector<char>& v);
+void ispisuje(vector<char>& v);
---
+++ Vjezba_4/zadatak_1/zadatak_1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Header.hpp"
+
+using namespace std;
+
+int main()
+{
+	vector<char> c;
+	razdvaja(c);
+	ispisuje(c);
+}
