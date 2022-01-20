5597f7bc0ea986fbef1d204f1e91436aa4d46cf5
pasko <stipandzijapasko@gmail.com>
2022-01-20 18:02:20 +0100

    Vjezba_10

---
+++ Vjezba_10/Zadatak_1/Zadatak_1/zadatak_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+template <class T>
+void sortiranje(vector<T>& v)
+{
+	sort(v.begin(), v.end());
+}
+
+template<>
+void sortiranje(vector<char>& v)
+{
+	for (int i = 0; i < (int)v.size(); i++)
+	{
+		char c = v[i];
+		if (isupper(v[i])) {
+			c = tolower(v[i]);
+			v[i] = c;
+		}
+	}
+	sort(v.begin(), v.end());
+}
+
+int main()
+{
+	vector<int> v1;
+	vector<char> v2;
+	for (int i = 0; i < 5; i++) {
+		cout << "Unesi broj: ";
+		int x; cin >> x;
+		cout << endl;
+		cout << "Unesi char: ";
+		char c; cin >> c;
+		cout << endl;
+		v1.push_back(x);
+		v2.push_back(c);
+	}
+
+	sortiranje(v1);
+	sortiranje<char>(v2);
+	for (int i = 0; i < (int)v1.size(); i++) {
+		cout << v1[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < (int)v2.size(); i++) {
+		cout << v2[i] << " ";
+	}
+
+
+
+
+
+}
