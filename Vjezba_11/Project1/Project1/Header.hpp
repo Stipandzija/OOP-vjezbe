d5349389880459369a46c50a5e30c4dce738174d
pasko <stipandzijapasko@gmail.com>
2022-01-28 00:44:02 +0100

    Vjezba_10

--- Vjezba_11/Project1/Project1/Header.hpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-
-void odbaci(vector<int>&);
-void prebroji(vector<int>&);
-void min_max(vector<int>&);
-void izbaci(vector<int>&);
-void sort(vector<int>&);
